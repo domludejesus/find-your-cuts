efc02d0340196a9e111fd1f02abee80cf8605097
Dominic DeJesus <domludejesus@gmail.com>
2022-08-20 20:57:03 -0400

    'adding map to ios device preview'

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Capacitor } from '@capacitor/core';
+import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native'
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  @ViewChild('map') mapView: ElementRef;
+  constructor() { }
 
+  ionViewDidEnter() {
+    this.createMap();
+  }
+
+  createMap() {
+    const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
+    console.log("~ file: tab2.page.ts ~ Line 21 ~ HomePage ~ createMap ~boundingRect", boundingRect)
+
+    CapacitorGoogleMaps.create({
+      width: Math.round(boundingRect.width),
+      height: Math.round(boundingRect.height),
+      x: Math.round(boundingRect.x),
+      y: Math.round(boundingRect.y),
+      //latitude?: Number,
+      //Longitude?: Number, 
+      zoom: 3,
+      //liteMode?: Boolean 
+    })
+  }
 }
