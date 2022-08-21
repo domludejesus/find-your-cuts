52effc173df7baed2d3aa69a33853801b582ef69
Dominic DeJesus <domludejesus@gmail.com>
2022-08-20 20:08:54 -0400

    'adding android capacitor/geolocation config'

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,14 @@
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
+import com.hemangkumar.capacitorgooglemaps.capacitorGoogleMaps;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); 
+
+        // ...
+        registerPlugin(capacitorGoogleMaps.class);
+    }
+}
